8792f343097a7c30807ceaabcf5942fa4936972f
Yago Oyarzabal <38369070+oyarzabalyago@users.noreply.github.com>
2018-07-11 22:17:42 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
@@ -1,4 +1,34 @@
 function mostrar()
 {
+// declaro las variables a usar    
+var dato1;
+var dato2;
+var resultado;
 
+// capturo los valores ingresados
+   dato1 = parseInt(prompt ("Ingrese un Numero por favor"));
+   dato2 = parseInt(prompt ("Ingrese otro por favor"));
+
+//comparo los numeros ingresados
+   if(dato1==dato2)
+   {
+       alert(dato1+''+dato2);
+   }
+   else if(dato1>dato2)
+   {
+       resultado = dato1 - dato2;
+       alert('El resultado es: '+ parseInt(resultado));
+   }
+   else
+   {
+        resultado = dato1 + dato2;
+        if(parseInt(resultado)>10)
+        {
+            alert("La suma es "+ resultado +" y supero el 10");
+        }
+        else
+        {
+            alert("La suma es "+ resultado);
+        }
+   }   
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,14 @@
-
 function mostrar()
 {
+// declaro las variables a usar    
+var dato1;
+var dato2;
+
+// capturo los valores ingresados
+   dato1 = parseInt(prompt ("Ingrese el largo del rectangulo"));
+   dato2 = parseInt(prompt ("Ingrese el ancho del rectangulo"));
+
+//comparo los numeros ingresados
+       alert("El perimetro es: "+ ((dato1 + dato2)*2));
 
 }
